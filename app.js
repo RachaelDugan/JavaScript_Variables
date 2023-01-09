8fea055362fc5f4bc205909e1ac9c23c09a59f97
RachaelDugan <118020235+RachaelDugan@users.noreply.github.com>
2023-01-09 12:08:34 -0700

    Initial commit

--- app.js
+++ app.js
@@ -3,8 +3,29 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = "Rachael";
+let lastName = "Dugan";
+var age = 44;
 
-// Exercise 2
+let language = "JavaAscript";
+let createdYear = 1995;
+let isCaseSensitive = true;
 
-// Exercise 3
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+selectSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookMark = 456;
+let hasRead = true;
+
+console.log(`${firstName} ${lastName} loves ${language}! 
+It has been around since ${createdYear} and created by ${author}. 
+You can buy his book called ${title} for the low price of ${price}. 
+I heard it was on sale but that was ${isOnSale}. It's only ${pageCount} pages but has lots of info. 
+They say that ${language} is case sensitive and that turns out to be ${isCaseSensitive}!`);
